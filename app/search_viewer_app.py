9ab1c0dcfc4fe765482bd4eb33d3d2a76f18010a
Giseop Kim <gisbi.kim@gmail.com>
2023-03-19 01:06:16 +0900

    gskim: refactoring the app

--- app/search_viewer_app.py
+++ app/search_viewer_app.py
@@ -5,125 +5,52 @@ import requests
 import datetime
 import json
 import re
+from search_viewer_app_lib import *
 
-# this must be at the top
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")  # this must be at the top
 
-# config.toml 파일 경로
-CONFIG_FILE = './.streamlit/config.toml'
+spawn_darkmode_sidebar()
+spawn_title()
+gray_line()
 
-# # config.toml 파일을 읽어옴
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+search_type, AND_or_OR = spawn_selection_options()
+gray_line()
 
-with st.sidebar:
-    config.read(CONFIG_FILE)
-    theme_option = st.radio("Select Light/Dark mode", ["Light", "Dark"])
-    if st.button("Double click to Rerun (Light/Dark)"):
-
-        if theme_option == "Light":
-            config.set('theme', 'base', '\"light\"')
-
-        if theme_option == "Dark":
-            config.set('theme', 'base', '\"dark\"')
-
-        with open(CONFIG_FILE, 'w') as f:
-            config.write(f)
-
-        st.cache_resource()
-
-# Create two columns
-col1, col2 = st.columns([1, 4])
+col1, col2 = st.columns([2, 1])
 with col1:
-    st.markdown("<p style='display: flex; justify-content: center'><img src='https://bpb-us-e1.wpmucdn.com/blogs.cornell.edu/dist/8/7752/files/2021/02/arxiv-logo-1.png' width='150'></p>", unsafe_allow_html=True)
-with col2:
-    st.title("Paper Search App")
+    keywords = spawn_search_zone()
 
-st.markdown("<hr>", unsafe_allow_html=True)
+if not all_keywords_has_more_including_than_k_len(keywords, 2):
+    st.write("Please enter at least two alphabets for each keyword.")
+else:
+    DEFAULT_NUM_PAPERS_PER_PAGE = 10
+    with col2:
+        page_size = st.selectbox("Select the number of papers per page", [
+            5, 10, 15, 20], index=1)
+    NUM_PAPERS_PER_PAGE = page_size or DEFAULT_NUM_PAPERS_PER_PAGE
+    st.write(f"🍀 {NUM_PAPERS_PER_PAGE} items per page")
 
-col1, col2 = st.columns([1, 2])
-with col1:
-    search_type = st.radio("Select the search type", [
-        "RO: Robotics", "CV: Computer Vision and Pattern Recognition",
-                        "AI: Artificial Intelligence", "LG: Machine Learning"])
-with col2:
-    AND_or_OR = st.radio("Select OR or AND", ["OR", "AND"])
+    table_to_search = search_type[:2]
+    json_data = get_data_from_server(table_to_search, AND_or_OR, keywords)
 
-st.markdown("<hr>", unsafe_allow_html=True)
+    total_count = len(json_data)
+    num_pages = (total_count - 1) // NUM_PAPERS_PER_PAGE + 1
 
-keywords = st.text_input("🔍 Enter keywords separated by commas")
-keywords = keywords.split(",")
-
-if all(len(keyword) >= 2 for keyword in keywords):
-
-    st.write("🍀 10 items per page")
-
-    search_type_key = search_type[:2]
-    url = f"http://127.0.0.1:8000/{search_type_key}/keywords/{AND_or_OR}/{','.join(keywords)}"
-    response = requests.get(url)
     try:
-        json_data = json.loads(response.text)
-        total_count = len(json_data)
-        page_size = 10
-        num_pages = (total_count - 1) // page_size + 1
-
         current_page = st.sidebar.number_input(
             "Enter a page number", min_value=1, max_value=num_pages, value=1
         )
-        start_index = (current_page - 1) * page_size
-        end_index = min(start_index + page_size, total_count)
+        start_index = (current_page - 1) * NUM_PAPERS_PER_PAGE
+        end_index = min(start_index + NUM_PAPERS_PER_PAGE, total_count)
 
-        col1, col2, col3, col4 = st.columns([0.5, 2, 5, 0.5])
-        with col1:
-            st.markdown(
-                f"<p style='text-align: center; font-weight: bold;'>Date</p>", unsafe_allow_html=True)
-        with col2:
-            st.markdown(
-                f"<p style='text-align: center; font-weight: bold;'>Title</p>", unsafe_allow_html=True)
-        with col3:
-            st.markdown(
-                f"<p style='text-align: center; font-weight: bold;'>Abstract</p>", unsafe_allow_html=True)
-        with col4:
-            st.markdown(
-                f"<p style='text-align: center; font-weight: bold;'>Link</p>", unsafe_allow_html=True)
+        spawn_column_names()
+        for i, item in enumerate(list(json_data.items())[start_index:end_index]):
+            spawn_item_row(item, keywords)
 
-        def highlight_keyword(text, keywords):
-            pattern = re.compile('|'.join(keywords), flags=re.IGNORECASE)
-            highlighted_text = pattern.sub(
-                lambda x: f"<mark style='background-color: #ffffb3;'>{x.group()}</mark>", text)
-            return highlighted_text
-
-        for i, (key, val) in enumerate(list(json_data.items())[start_index:end_index]):
-            col1, col2, col3, col4 = st.columns([0.5, 2, 5, 0.5])
-            created_at = str(val[0])
-            summary = val[1]
-            link = val[2]
-            with col1:
-                st.markdown(
-                    f"<p style='text-align: center'>{created_at[:4]}<br>{created_at[5:10]}</p>", unsafe_allow_html=True)
-            with col2:
-                highlighted_key = highlight_keyword(key, keywords)
-                st.markdown(
-                    f"<div style='font-weight: bold;'>{highlighted_key}</div>", unsafe_allow_html=True)
-            with col3:
-                highlighted_summary = highlight_keyword(summary, keywords)
-                st.markdown(highlighted_summary, unsafe_allow_html=True)
-            with col4:
-                st.write(link)
-
-        st.sidebar.markdown(
-            f"Page {current_page} of {num_pages}")
+        spawn_sidebar_md(f"Page {current_page} of {num_pages}")
 
     except ValueError as e:
         st.write("Error loading JSON:", e)
-else:
-    st.write("Please enter at least two alphabets for each keyword.")
 
-
-st.markdown("<hr>", unsafe_allow_html=True)
-
-st.write("Author: gisbi.kim@gmail.com")
-
-last_modified = os.path.getmtime(__file__)
-st.write("Last update: ", datetime.datetime.fromtimestamp(
-    last_modified).strftime("%Y-%m-%d"))
+gray_line()
+spawn_info()
---
+++ app/search_viewer_app_lib.py
@@ -0,0 +1,136 @@
+import os
+import streamlit as st
+import configparser
+import requests
+import datetime
+import json
+import re
+
+
+def spawn_darkmode_sidebar():
+    # config.toml 파일 경로
+    CONFIG_FILE = './.streamlit/config.toml'
+
+    # # config.toml 파일을 읽어옴
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    with st.sidebar:
+        config.read(CONFIG_FILE)
+        theme_option = st.radio("Select Light/Dark mode", ["Light", "Dark"])
+        if st.button("Double-click to change (Light/Dark)"):
+
+            if theme_option == "Light":
+                config.set('theme', 'base', '\"light\"')
+
+            if theme_option == "Dark":
+                config.set('theme', 'base', '\"dark\"')
+
+            with open(CONFIG_FILE, 'w') as f:
+                config.write(f)
+
+            st.cache_resource()
+
+
+def spawn_title():
+    # Create two columns
+    col1, col2 = st.columns([1, 4])
+    with col1:
+        img_src = "<img src='https://bpb-us-e1.wpmucdn.com/blogs.cornell.edu/dist/8/7752/files/2021/02/arxiv-logo-1.png' width='170'>"
+        st.markdown(f"<p style='display: flex; justify-content: center'>{img_src}</p>",
+                    unsafe_allow_html=True)
+    with col2:
+        st.title("Paper Search App")
+
+
+def gray_line():
+    st.markdown("<hr>", unsafe_allow_html=True)
+
+
+def spawn_selection_options():
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        search_type = st.radio("Select the search type", [
+            "RO: Robotics",
+            "CV: Computer Vision and Pattern Recognition",
+            "AI: Artificial Intelligence",
+            "LG: Machine Learning"])
+    with col2:
+        AND_or_OR = st.radio("Select OR or AND", ["OR", "AND"])
+
+    return search_type, AND_or_OR
+
+
+def spawn_search_zone():
+    keywords = st.text_input("🔍 Enter keywords separated by commas")
+    keywords = keywords.split(",")
+    return keywords
+
+
+def get_data_from_server(table_to_search, AND_or_OR, keywords):
+    url = f"http://127.0.0.1:8000/{table_to_search}/keywords/{AND_or_OR}/{','.join(keywords)}"
+    response = requests.get(url)
+    return json.loads(response.text)
+
+
+def all_keywords_has_more_including_than_k_len(keywords, k):
+    return all(len(keyword) >= k for keyword in keywords)
+
+
+def spawn_column_names():
+    col1, col2, col3, col4 = st.columns([0.5, 2, 5, 0.5])
+    with col1:
+        st.markdown(
+            f"<p style='text-align: center; font-weight: bold;'>Date</p>", unsafe_allow_html=True)
+    with col2:
+        st.markdown(
+            f"<p style='text-align: center; font-weight: bold;'>Title</p>", unsafe_allow_html=True)
+    with col3:
+        st.markdown(
+            f"<p style='text-align: center; font-weight: bold;'>Abstract</p>", unsafe_allow_html=True)
+    with col4:
+        st.markdown(
+            f"<p style='text-align: center; font-weight: bold;'>Link</p>", unsafe_allow_html=True)
+
+
+def highlight_keyword(text, keywords):
+    pattern = re.compile('|'.join(keywords), flags=re.IGNORECASE)
+    highlighted_text = pattern.sub(
+        lambda x: f"<mark style='background-color: #ffffb3;'>{x.group()}</mark>", text)
+    return highlighted_text
+
+
+def spawn_item_row(item, keywords):
+    key, val = item
+    created_at = str(val[0])
+    created_at_yyyy = created_at[:4]
+    created_at_mmdd = created_at[5:10]
+    summary = val[1]
+    link = val[2]
+
+    col1, col2, col3, col4 = st.columns([0.5, 2, 5, 0.5])
+    with col1:
+        st.markdown(
+            f"<p style='text-align: center'>{created_at_yyyy}<br>{created_at_mmdd}</p>", unsafe_allow_html=True)
+    with col2:
+        highlighted_key = highlight_keyword(key, keywords)
+        st.markdown(
+            f"<div style='font-weight: bold;'>{highlighted_key}</div>", unsafe_allow_html=True)
+    with col3:
+        highlighted_summary = highlight_keyword(summary, keywords)
+        st.markdown(highlighted_summary, unsafe_allow_html=True)
+    with col4:
+        st.write(
+            f"<div style='text-align: center;'><a href='{link}'>arxiv</a></div>", unsafe_allow_html=True)
+
+
+def spawn_sidebar_md(msg):
+    st.sidebar.markdown(msg)
+
+
+def spawn_info():
+    st.write("Author: gisbi.kim@gmail.com")
+
+    last_modified = os.path.getmtime(__file__)
+    st.write("Last update: ", datetime.datetime.fromtimestamp(
+        last_modified).strftime("%Y-%m-%d"))
